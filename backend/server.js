feb492f51d269700b635577e3d9795aa8540c72e
Mitchellmbc <93491911+Mitchellmbc@users.noreply.github.com>
2022-04-28 14:03:25 +0100

    refactored to use Axios

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@
 
 import express from 'express';
 
+import cors from 'cors';
+
 //const data = require('./data.js');
 
 import data from './data.js';
 
 const app = express();
+app.use(cors());
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);

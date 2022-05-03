90971cd0476527bc588e6f83fac023bd05fbe500
Mitchellmbc <93491911+Mitchellmbc@users.noreply.github.com>
2022-05-03 09:06:27 +0100

    xD

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,14 +1,31 @@
 const Rating = {
   render: (props) => {
-    if(props.value) {
+    if(!props.value) {
       return '<div></div>';
     }
     return `
     <div class="rating">
       <span>
-        <i class="${props.vaue >= 1 ? `fa fa-star` : props.value >= 0 ? 'fa fa-star-half-o' : 'fa fa-star-o'}">
+        <i class="${props.value >= 1 ? `fa fa-star` : props.value >= 0 ? 'fa fa-star-half-o' : 'fa fa-star-o'}">
         </i>
       </span>
+      <span>
+        <i class="${props.value >= 2 ? `fa fa-star` : props.value >= 1.5 ? 'fa fa-star-half-o' : 'fa fa-star-o'}">
+        </i>
+      </span>
+      <span>
+        <i class="${props.value >= 3 ? `fa fa-star` : props.value >= 2.5 ? 'fa fa-star-half-o' : 'fa fa-star-o'}">
+        </i>
+      </span>
+      <span>
+        <i class="${props.value >= 4 ? `fa fa-star` : props.value >= 3.5 ? 'fa fa-star-half-o' : 'fa fa-star-o'}">
+        </i>
+      </span>
+      <span>
+        <i class="${props.value >= 5 ? `fa fa-star` : props.value >= 4.5 ? 'fa fa-star-half-o' : 'fa fa-star-o'}">
+        </i>
+      </span>
+      <span> ${props.text || ''} </span>
     </div>
     `
   }

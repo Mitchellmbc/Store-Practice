59b6b46ee6564ffa0993c41a0e4937f0941eadba
Mitchellmbc <93491911+Mitchellmbc@users.noreply.github.com>
2022-05-04 08:23:37 +0100

    .

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,14 @@
+import {parseRequestUrl} from '../utils';
+import { getProduct } from '../api';
 
 const ProductScreen = {
-  render: () => {
-    return `<div>ProductScreen</div>`;
+  render: async () => {
+    const request = parseRequestUrl();
+    const product = await getProduct(request.id);
+    console.log(request);
+    console.log(product);
+    console.log(product.name);
+    return `<h1>${product.name}</h1>`;
   },
 };
 
